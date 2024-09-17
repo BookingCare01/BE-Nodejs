c474bcb612c7fd8f109984993e8e4bdbc0320bb5
MinhSang97 <minhsangnguyen463@gmail.com>
2024-09-17 09:56:51 +0700

    connect DB

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,18 +1,22 @@
+require('dotenv').config(); // Load biến môi trường từ .env
 const { Sequelize } = require('sequelize');
 
+// Lấy URL kết nối từ biến môi trường
+const databaseUrl = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize('hoidanit', 'root', '123456', {
-    host: 'localhost',
+// Tạo instance Sequelize từ URL kết nối
+const sequelize = new Sequelize(databaseUrl, {
     dialect: 'mysql',
     logging: false
 });
 
-let connectDB = async() => {
+let connectDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
-}
-module.exports = connectDB;''
+};
+
+module.exports = connectDB;
